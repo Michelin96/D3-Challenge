5b1cf3c56a986beaa7c604f2a578e9b41d444c51
MichelleH <voirdire96@gmail.com>
2020-10-13 21:32:07 -0700

    Made steps of code to modify

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,3 +1,30 @@
+// Set the margins
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+    .select(".chart")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Get the data with a promise and fulfillment
 d3.csv("assets/data/data.csv").then(function(riskData) {
     console.log(riskData);
 
@@ -5,36 +32,47 @@ d3.csv("assets/data/data.csv").then(function(riskData) {
     let stateAbbr = riskData.map(data => data.abbr);
     console.log("State Abbrevation", stateAbbr);
 
-    var svgWidth = 960;
-    var svgHeight = 500;
+    //Parse Data/Cast as numbers
+    riskData.forEach(data => {
+        data.age = +data.age;
+        data.income = +data.income;
+      });
+  
+    // Create scale functions
+    var xLinearScale = d3.scaleLinear()
+        .domain([20, d3.max(hairData, d => d.hair_length)])
+        .range([0, width]);
+    
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(hairData, d => d.num_hits)])
+        .range([height, 0]);
+    
+    // Create axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
 
-    var margin = {
-        top: 20,
-        right: 40,
-        bottom: 80,
-        left: 100
-    };
+    // Append Axes to the chart
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
 
-    var width = svgWidth - margin.left - margin.right;
-    var height = svgHeight - margin.top - margin.bottom;
+    chartGroup.append("g")
+        .call(leftAxis);
 
-    // Create an SVG wrapper, append an SVG group that will hold our chart,
-    // and shift the latter by left and top margins.
-    var svg = d3
-        .select(".chart")
-        .append("svg")
-        .attr("width", svgWidth)
-        .attr("height", svgHeight);
+    // Create Circles
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(hairData)
+        .join("circle")
+        .attr("cx", d => xLinearScale(d.hair_length))
+        .attr("cy", d => yLinearScale(d.num_hits))
+        .attr("r", "15")
+        .attr("fill", "pink")
+        .attr("opacity", 0.5)
+        .attr("stroke", "black")
+        .attr("stroke-width", 1);
 
-    // Append an SVG group
-    var chartGroup = svg.append("g")
-        .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-    // Cast each hours value in tvData as a number using the unary + operator
-    // tvData.forEach(data => {
-    //   data.hours = +data.hours;
-    //   console.log("Name:", data.name);
-    //   console.log("Hours:", data.hours);
-    // });
+    // Add State Abbrevations to each scatter point
 
 }).catch(error => console.log(error));
+
+    
