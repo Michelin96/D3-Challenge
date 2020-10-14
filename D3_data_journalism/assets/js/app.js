e4296941db3e91acc280a0cf98bb85bddbd856d3
MichelleH <voirdire96@gmail.com>
2020-10-13 20:16:24 -0700

    Imported csv

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,17 @@
-// @TODO: YOUR CODE HERE!
+d3.csv("assets/data/data.csv").then(function(riskData) {
+
+    console.log(riskData);
+
+    // // log a list of names
+    // var names = tvData.map(data => data.name);
+    // console.log("names", names);
+
+    // // Cast each hours value in tvData as a number using the unary + operator
+    // tvData.forEach(data => {
+    //   data.hours = +data.hours;
+    //   console.log("Name:", data.name);
+    //   console.log("Hours:", data.hours);
+    // });
+
+});
+//.catch(error => console.log(error));
